9c72074e15d2a9ca7a094b8e6ff776ed50f6e05c
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-02 15:28:54 +0200

    migrate to src folder structure

--- next.config.js
+++ next.config.js
@@ -1,25 +1,14 @@
-const million = require("million/compiler");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ["utfs.io"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["cdn.builder.io", "media.licdn.com"],
-  },
+  }
 };
 
-const millionConfig = {
-  auto: {
-    threshold: 0.05, // default: 0.1,
-    skip: ["useBadHook", /badVariable/g], // default []
-    // if you're using RSC: auto: { rsc: true },
-  },
-};
-
-module.exports = million.next(nextConfig, millionConfig);
+module.exports = nextConfig;

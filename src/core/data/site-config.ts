5b99c38765a560434ff450642dc823ae1a0e73db
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-03 00:31:04 +0200

    Add site config and fionts

---
+++ src/core/data/site-config.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Remco Stoeten - Snippets and blogs for remcostoeten.com',
+  description: 'Just some personal metrics, tools, and what not.',
+  links: {
+    github: 'https://github.com/remcostoeten',
+    docs: 'https://snippets.remcostoeten.com',
+  },
+  url: 'https://blog.remcostoeten.com',
+};

8b041601193377dcb0cecf5db1b4ba53af34399a
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-04 19:53:25 +0200

    format

--- core/lib/utils.ts
+++ core/lib/utils.ts
@@ -3,12 +3,12 @@ import { customAlphabet } from "nanoid";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function generateId({ length = 8, prefix = "" } = {}) {
-    return `${prefix}${customAlphabet(
-        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-        length,
-    )()}`;
+  return `${prefix}${customAlphabet(
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    length,
+  )()}`;
 }

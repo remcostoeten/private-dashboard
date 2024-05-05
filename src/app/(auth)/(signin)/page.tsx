c2078156e33af94d5493f114d361d11ed843be83
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-05 03:03:26 +0200

    attempt to build

--- src/app/(auth)/(signin)/page.tsx
+++ src/app/(auth)/(signin)/page.tsx
@@ -6,64 +6,64 @@ import { cn } from "@/core/lib/utils";
 import Link from "next/link";
 
 export default function AuthenticationPage() {
-    return (
-        <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <Link
-                href="/examples/authentication"
-                className={cn(
-                    buttonVariants({ variant: "ghost" }),
-                    "absolute right-4 hidden top-4 md:right-8 md:top-8",
-                )}
-            >
-                Login
-            </Link>
-            <div className="relative hidden h-full flex-col text-white dark:border-r lg:flex w-1/3 overflow-hidden">
-                <div className="relative z-20 flex  text-lg font-medium">
-                    <Logo
-                        className="absolute left-1/2 top-4 transform -translate-x-1/2 w-44"
-                        height="100"
-                    />
-                </div>
-                <video
-                    className="w-full h-full object-cover -z-10"
-                    src="login-video.mp4"
-                    autoPlay
-                    muted
-                    loop
-                >
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-            <div className="p-4 lg:p-8 h-full flex items-center ">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                    <div className="flex flex-col space-y-2 text-center">
-                        <h1 className="text-2xl font-semibold tracking-tight">
-                            Create an account
-                        </h1>
-                        <p className="text-sm text-muted-foreground">
-                            Enter your email below to create your account
-                        </p>
-                    </div>
-                    <UserAuthForm />
-                    <p className="px-8 text-center text-sm text-muted-foreground">
-                        By clicking continue, you agree to our{" "}
-                        <Link
-                            href="/terms"
-                            className="underline underline-offset-4 hover:text-primary"
-                        >
-                            Terms of Service
-                        </Link>{" "}
-                        and{" "}
-                        <Link
-                            href="/privacy"
-                            className="underline underline-offset-4 hover:text-primary"
-                        >
-                            Privacy Policy
-                        </Link>
-                        .
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="/examples/authentication"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 hidden top-4 md:right-8 md:top-8",
+        )}
+      >
+        Login
+      </Link>
+      <div className="relative hidden h-full flex-col text-white dark:border-r lg:flex w-1/3 overflow-hidden">
+        <div className="relative z-20 flex  text-lg font-medium">
+          <Logo
+            className="absolute left-1/2 top-4 transform -translate-x-1/2 w-44"
+            height="100"
+          />
         </div>
-    );
+        <video
+          className="w-full h-full object-cover -z-10"
+          src="login-video.mp4"
+          autoPlay
+          muted
+          loop
+        >
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="p-4 lg:p-8 h-full flex items-center ">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }

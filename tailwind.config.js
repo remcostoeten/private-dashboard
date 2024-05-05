9d14a9a55c840816177aaa88b8385cd73af874b9
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-05 03:03:26 +0200

    attempt to build

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,14 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
         "accordion-down": {
           from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -69,6 +77,7 @@ module.exports = {
         },
       },
       animation: {
+        shimmer: "shimmer 2s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },

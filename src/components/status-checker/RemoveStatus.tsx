dd7f75d58912604ae241991f01184171bf7730dd
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-14 00:08:14 +0200

    format

--- src/components/status-checker/RemoveStatus.tsx
+++ src/components/status-checker/RemoveStatus.tsx
@@ -4,29 +4,29 @@ import { toast } from "sonner";
 import { Button } from "../ui/button";
 
 const ClearButton = () => {
-    const handleClear = async () => {
-        try {
-            const response = await fetch("/api/status/clear/", { method: "POST" });
-            if (!response.ok) {
-                throw new Error("Failed to clear status data.");
-            }
-            const data = await response.json();
-            toast("Data clearend successfully");
-            console.log(data.message);
-        } catch (error) {
-            toast("Failed to clear status data");
-            console.error(error);
-        }
-    };
+  const handleClear = async () => {
+    try {
+      const response = await fetch("/api/status/clear/", { method: "POST" });
+      if (!response.ok) {
+        throw new Error("Failed to clear status data.");
+      }
+      const data = await response.json();
+      toast("Data clearend successfully");
+      console.log(data.message);
+    } catch (error) {
+      toast("Failed to clear status data");
+      console.error(error);
+    }
+  };
 
-    return (
-        <Button variant="secondary" onClick={handleClear}>
-            <span className="flex gap-2">
-                <Trash2 />
-                Clear Status Data
-            </span>
-        </Button>
-    );
+  return (
+    <Button variant="secondary" onClick={handleClear}>
+      <span className="flex gap-2">
+        <Trash2 />
+        Clear Status Data
+      </span>
+    </Button>
+  );
 };
 
 export default ClearButton;

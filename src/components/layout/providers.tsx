9fc6fa3de668e6bfb35b4083d9f11f8347581ce9
remco stoeten <remcostoeten@hotmail.com>
2024-05-17 03:43:02 +0200

    Add provider

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,14 +1,28 @@
 "use client";
-import React from "react";
+import React, { ReactNode } from "react";
 import ThemeProvider from "./ThemeToggle/theme-provider";
 import { AuthContextProvider } from "@/core/providers/auth-provider";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  })
+}
+
+type ProvidersProps = {
+  children?: ReactNode;
+};
+
+export default function Providers({ children }: ProvidersProps) {
   return (
     <AuthContextProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <TooltipProvider>{children}</TooltipProvider>
+        <PostHogProvider client={posthog}>
+          <TooltipProvider>{children}</TooltipProvider>
+        </PostHogProvider>
       </ThemeProvider>
     </AuthContextProvider>
   );

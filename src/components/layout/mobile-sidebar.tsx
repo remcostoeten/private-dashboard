3eea1a52bf7d50166586efe1273562555afae766
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-21 23:17:28 +0200

    install prosetypography

--- src/components/layout/mobile-sidebar.tsx
+++ src/components/layout/mobile-sidebar.tsx
@@ -8,30 +8,27 @@ import { useState } from 'react'
 // import { Playlist } from "../data/playlists";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  // playlists: Playlist[];
+    // playlists: Playlist[];
 }
 
 export function MobileSidebar({ className }: SidebarProps) {
-  const [open, setOpen] = useState(false)
-  return (
-    <>
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger asChild>
-          <MenuIcon />
-        </SheetTrigger>
-        <SheetContent side="left" className="!px-0">
-          <div className="space-y-4 py-4">
-            <div className="px-3 py-2">
-              <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-                Overview
-              </h2>
-              <div className="space-y-1">
-                <DashboardNav items={navItems} setOpen={setOpen} />
-              </div>
-            </div>
-          </div>
-        </SheetContent>
-      </Sheet>
-    </>
-  )
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            <Sheet open={open} onOpenChange={setOpen}>
+                <SheetTrigger asChild>
+                    <MenuIcon />
+                </SheetTrigger>
+                <SheetContent side="left" className="!px-0">
+                    <div className="space-y-4 py-4">
+                        <div className="px-3 py-2">
+                            <div className="space-y-1">
+                                <DashboardNav items={navItems} setOpen={setOpen} />
+                            </div>
+                        </div>
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </>
+    )
 }
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,20 +3,17 @@ import { navItems } from '@/core/constants/data'
 import { cn } from '@/core/lib/utils'
 
 export default function Sidebar() {
-  return (
-    <nav
-      className={cn(`relative hidden h-screen border-r pt-16 lg:block w-72`)}
-    >
-      <div className="space-y-4 py-4">
-        <div className="px-3 py-2">
-          <div className="space-y-1">
-            <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
-              Overview
-            </h2>
-            <DashboardNav items={navItems} />
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
+    return (
+        <nav
+            className={cn(`relative hidden h-screen border-r pt-16 lg:block w-72`)}
+        >
+            <div className="space-y-4 py-4">
+                <div className="px-3 py-2">
+                    <div className="space-y-1">
+                        <DashboardNav items={navItems} />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }

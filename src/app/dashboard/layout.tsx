1710599ee55629c4ffae4ed0c892fe686ebedc3e
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-23 19:13:58 +0200

    rm profile

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,19 +4,19 @@ import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-  title: 'Remco Stoeten - Dashboard',
+    title: 'Remco Stoeten - Dashboard',
 }
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <Header />
-      <div className="flex ">
-        <Sidebar />
-        <main className=" md:gap-8 flex-1 ">
-          <div className="pb-8">{children}</div>
-        </main>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className="flex ">
+                <Sidebar />
+                <main className="md:gap-8 flex-1 ml-7 mt-20">
+                    <div className="pb-8">{children}</div>
+                </main>
+            </div>
+        </>
+    )
 }

a2f697cfca48d06f6ef5e9e0298a107d31ac1c05
remco stoeten <remcostoeten@hotmail.com>
2024-05-27 02:38:25 +0200

    jerge

--- src/core/database/firebase.ts
+++ src/core/database/firebase.ts
@@ -13,14 +13,18 @@ import {
   doc,
   setDoc,
 } from 'firebase/firestore'
-
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCZdnGJtM7rxzk7y7Ga1vKvGoHv8ja23e4',
+
+  authDomain: 'personal-panel---chat.firebaseapp.com',
+
+  projectId: 'personal-panel---chat',
+
+  storageBucket: 'https://github.com/DivMagicCom/DivMagicpersonal-panel---chat.appspot.com',
+
+  messagingSenderId: '534740783847',
+
+  appId: '1:534740783847:web:2ba1ee44119c4d862a4842',
 }
 
 if (!getApps().length) {
